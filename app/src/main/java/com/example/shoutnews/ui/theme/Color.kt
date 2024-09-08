94c2efb4bdd56e82c51e8ebea2e750333449e35d
MohammadNikmard <mnikmard2004@gmail.com>
2024-09-08 10:45:38 +0330

    feature: add colors and fonts

--- app/src/main/java/com/example/shoutnews/ui/theme/Color.kt
+++ app/src/main/java/com/example/shoutnews/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.example.shoutnews.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val purpleDark = Color(0xff2536A7)
+val purplePrimary = Color(0xff475AD7)
+val purpleLight = Color(0xff8A96E5)
+val purpleLighter = Color(0xffEEF0FB)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val blackDark = Color(0xff22242F)
+val blackPrimary = Color(0xff333647)
+val blackLight = Color(0xff44485F)
+val blackLighter = Color(0xff555A77)
+
+val greyDark = Color(0xff666C8E)
+val greyPrimary = Color(0xff7C82A1)
+val greyLight = Color(0xffACAFC3)
+val greyLighter = Color(0xffF3F4F6)
